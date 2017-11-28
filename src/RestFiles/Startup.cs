2d7369b7309df536044701e5954f58da6a08991c
Demis Bellot <demis.bellot@gmail.com>
2017-11-27 19:43:27 -0500

    Inject configuration

--- src/RestFiles/Startup.cs
+++ src/RestFiles/Startup.cs
@@ -7,8 +7,9 @@ using Funq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using RestFiles.ServiceInterface;
 using ServiceStack;
 using ServiceStack.Configuration;
@@ -17,6 +18,9 @@ namespace RestFiles
 {
     public class Startup
     {
+        IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -33,7 +37,9 @@ namespace RestFiles
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseServiceStack(new AppHost());
+            app.UseServiceStack(new AppHost {
+                AppSettings = new NetCoreAppSettings(Configuration)
+            });
         }
     }
 
